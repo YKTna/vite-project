47e645b27ee95e28624753a64f66acd7653d79d4
Nikolay Eremeev <nikolay@guesst.co>
2026-06-08 14:18:12 +0300

    Новыцй

--- src/store/task.tsx
+++ src/store/task.tsx
@@ -1,42 +1,47 @@
 import { createSlice, Dispatch } from '@reduxjs/toolkit'
 import { RootState } from './store.tsx'
 
-export interface CounterState {
-  value: number
+export interface TaskState {
+  title: string,
+
 }
 
-const initialState: CounterState = {
-  value: 0, // Начальное состояние 0
+
+
+
+
+const initialState: TaskState = {
+  title: 'Заголовок', // Начальное состояние 0
+
 }
 
-export const counterSlice = createSlice({
-  name: 'counter', // Название слайса
+
+
+
+
+export const taskSlice = createSlice({
+  name: 'task', // Название слайса
   initialState, // Начальное состояние которое передаем
   reducers: { // Функции которые позволяют изменять состояниее
-    increment: (state, action: { payload: number }) => { // Обращаемся через state
-      state.value += action.payload;
-    },
-    decrement: (state, action: { payload: number}) => {
-      state.value -= action.payload;
-    },
+    setTaskTitle: (state, action: { payload: string }) => { // Обращаемся через state
+      state.title = action.payload;
+    }
   },
 })
 
-export const getCount = (state: RootState) => state.counter.value;
 
-// Каррирование функций
-// Такая функци позволяется диспатчить функции вместо простых экшен объектов
-// Первая функция внешняя - принимает value, которое мы передаем с помощью dispatch
-// Вторая функция внутренняя - вызывается редаксом
-export const incrementBy = (value: number) => (dispatch: Dispatch) => {
-  dispatch(increment(value)) // Тело внутренней функции
-  // Отправляет action в редакс хранилище, чтобы reducers его обработал и увеличил состояние
-}
 
-export const decrementBy = (value: number) => (dispatch: Dispatch) => {
-  dispatch(decrement(value))
-}
+export const getTitle = (state: RootState) => state.task.title;
 
-export const { increment, decrement } = counterSlice.actions;
 
-export default counterSlice.reducer
+
+
+
+
+export const { setTaskTitle } = taskSlice.actions;
+
+
+
+
+
+export default taskSlice.reducer
