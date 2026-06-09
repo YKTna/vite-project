263f53b52f7acaf4cac5c2bd2b670ebeaf2bda72
Nikolay Eremeev <nikolay@guesst.co>
2026-06-09 14:49:55 +0300

    Добавила сортировку, поиск по списку

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,20 +1,95 @@
+import classes from './Home.module.css'
 import { useAppDispatch, useAppSelector } from "../../hooks/redux.hooks.tsx"
-import { getTitle, setTaskTitle } from "../../store/task.tsx"
+import { getArrTable, getTitle, addTask, setTaskTitle, deleteTask, sortingTypeNumber, sortingNumber, sortingTypeName, sortingName, ArrTable } from "../../store/task.tsx"
+import { useEffect, useState } from 'react'
 
 
 export default function Home() {
+  const [addInputValue, setAddInputValue] = useState('')
+  const [filteredValue, setFilteredValue] = useState('');
+  const [filteredArray, setFilteredArray] = useState<ArrTable[]>([]);
   const title = useAppSelector(getTitle)
   const dispatch = useAppDispatch()
-  
   const handletitle = () => {
     dispatch(setTaskTitle('влмтдвмтл'))
   }
+  const arrTable = useAppSelector(getArrTable)
+  const handleArr = () => {
+    dispatch(addTask(addInputValue))
+    setAddInputValue('')
+  }
+  const handleDelete = (id: number) => {
+    dispatch(deleteTask(id))
+  }
+
+  const sortingTypeN = useAppSelector(sortingTypeNumber)
+  const handleSortedNumber = () => {
+    dispatch(sortingNumber(sortingTypeN))
+  }
+
+  const sortingTypeNam = useAppSelector(sortingTypeName)
+  const handleSortedName = () => {
+    dispatch(sortingName(sortingTypeNam))
+    console.log(sortingTypeNam)
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilteredValue(e.target.value)
+  };
+
+  useEffect(() => {
+    if(filteredValue != '') {
+      setFilteredArray(arrTable.filter((el) => el.name.toLowerCase().includes(filteredValue.toLowerCase())))
+    } else {
+      setFilteredArray(arrTable)
+    }
+  }, [filteredValue, arrTable])
+
+  const rows = filteredArray.map(function(item) {
+    return <tr key={item.id}>
+      <td className={classes.border}>{item.id}</td>
+      <td className={classes.border}>{item.name}</td>
+      <td className={classes.border}>
+        <button onClick={() => handleDelete(item.id)}>Удалить</button>
+      </td>
+    </tr>
+  })
 
   return (
     <div>
       <h1>{title}</h1>
-      <button onClick={handletitle}>Поменять</button>
-
+      <button onClick={handletitle} className={classes.taskButton}>Поменять Заголовок</button>
+      <div className={classes.divContainer}>
+        <button className={classes.taskButton} onClick={handleSortedNumber}>Сортировка по номеру</button>
+        <button className={classes.taskButton} onClick={handleSortedName}>Сортировка по названию</button>
+      </div>
+      <div className={classes.divInput}>
+        <h2>Поиск по списку</h2>
+        <input 
+        className={classes.taskInput}
+        onChange={handleChange}
+        placeholder="Поиск по задачам..." />
+      </div>
+      <table className={classes.table}>
+        <thead>
+          <tr>
+            <td className={`${classes.border} ${classes.bold}`}>Номер</td>
+            <td className={`${classes.border} ${classes.bold}`}>Название</td>
+            <td className={`${classes.border} ${classes.bold}`}>Удаление</td>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+      <div className={classes.addTask}>
+        <input 
+        value={addInputValue} 
+        onChange={(e) => setAddInputValue(e.target.value)} 
+        className={classes.taskInput}
+        placeholder="Новая задача..."/>
+        <button onClick={handleArr} className={classes.taskButton}>Добавить</button>
+      </div>
     </div>
   )
 }
--- src/store/task.tsx
+++ src/store/task.tsx
@@ -1,9 +1,17 @@
 import { createSlice, Dispatch } from '@reduxjs/toolkit'
 import { RootState } from './store.tsx'
 
+export interface ArrTable {
+  id: number,
+  name: string
+}
+
+
 export interface TaskState {
   title: string,
-
+  arrTable: ArrTable[],
+  sortingTypeNumber: boolean,
+  sortingTypeName: boolean
 }
 
 
@@ -12,7 +20,15 @@ export interface TaskState {
 
 const initialState: TaskState = {
   title: 'Заголовок', // Начальное состояние 0
-
+  arrTable: [
+    {id: 1, name: 'Один'},
+    {id: 2, name: 'Два'},
+    {id: 3, name: 'Три'},
+    {id: 4, name: 'Четыре'},
+    {id: 5, name: 'Пять'}
+  ],
+  sortingTypeNumber: true,
+  sortingTypeName: true
 }
 
 
@@ -25,20 +41,59 @@ export const taskSlice = createSlice({
   reducers: { // Функции которые позволяют изменять состояниее
     setTaskTitle: (state, action: { payload: string }) => { // Обращаемся через state
       state.title = action.payload;
-    }
+    },
+    addTask: (state, action: { payload: string }) => { // Обращаемся через state
+      if(action.payload != '') {
+        const newArr = [...state.arrTable]
+        const obj = {id: newArr.length + 1, name: action.payload}
+        newArr.push(obj)
+        state.arrTable = newArr
+      }
+    },
+    deleteTask: (state, action: { payload: number }) => { // Обращаемся через state
+      const newDeletedArr = state.arrTable.filter((el) => el.id != action.payload)
+      state.arrTable = newDeletedArr
+    },
+    sortingNumber: (state, action: { payload: boolean }) => { // Обращаемся через state
+      if(state.sortingTypeNumber == true) {
+        const arrCopy = [...state.arrTable]
+        const res = arrCopy.sort((a, b) => a.id - b.id)
+        state.arrTable = res
+        state.sortingTypeNumber = false
+      } else {
+        const arrCopy = [...state.arrTable]
+        const res = arrCopy.sort((a, b) => a.id - b.id).reverse()
+        state.arrTable = res
+        state.sortingTypeNumber = true
+      }
+    },
+    sortingName: (state, action: { payload: boolean }) => { // Обращаемся через state
+      if(state.sortingTypeName == true) {
+        const arrCopy = [...state.arrTable]
+        const res = arrCopy.sort((a, b) => a.name.localeCompare(b.name))
+        state.arrTable = res
+        state.sortingTypeName = false
+      } else {
+        const arrCopy = [...state.arrTable]
+        const res = arrCopy.sort((a, b) => a.name.localeCompare(b.name)).reverse()
+        state.arrTable = res
+        state.sortingTypeName = true
+      }
+    },
   },
 })
 
 
 
 export const getTitle = (state: RootState) => state.task.title;
+export const getArrTable = (state: RootState) => state.task.arrTable;
+export const sortingTypeNumber = (state: RootState) => state.task.sortingTypeNumber;
+export const sortingTypeName = (state: RootState) => state.task.sortingTypeName;
 
 
 
 
-
-
-export const { setTaskTitle } = taskSlice.actions;
+export const { setTaskTitle, addTask, deleteTask, sortingNumber, sortingName } = taskSlice.actions;
 
 
 
